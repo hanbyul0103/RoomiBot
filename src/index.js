db514b9f412acb73e84415c34d7efc1d876f838f
all007b <all007b@naver.com>
2024-07-26 19:54:44 +0900

    feat: bot setting

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const config = require('./config/config.json');
+const { Client, IntentsBitField } = require('discord.js');
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+client.on('ready', (c) => {
+    console.log(`[!] ${c.user.tag} is ready.`);
+});
+
+client.login(config.token);

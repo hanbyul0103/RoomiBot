340350679808e815d5d3975beabf8d76ca61d958
all007b <all007b@naver.com>
2024-08-04 09:52:47 +0900

    fix: data.json

--- src/commands/misc/calendarManage.js
+++ src/commands/misc/calendarManage.js
@@ -1,26 +1,6 @@
-const { ApplicationCommandOptionType, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
-const { createCanvas, registerFont } = require('canvas');
-const path = require('path');
-const fs = require('fs');
+const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
 const moment = require('moment-timezone');
-
-// 폰트 등록
-registerFont(path.join(__dirname, '../../../Fonts', 'NanumGothic.ttf'), { family: 'Nanum Gothic' });
-
-// 캘린더 데이터
-let calendarData = {};
-
-// 제목과 헤더의 크기 정의
-const scaleFactor = 3;
-const titleHeight = 140 * scaleFactor; // 제목의 공간을 140으로 설정
-const headerHeight = 100 * scaleFactor;
-
-const colorMap = {
-    'e': 'rgb(100, 255, 100)', // 이벤트 타입 e에 사용
-    'f': 'rgb(255, 100, 100)', // 이벤트 타입 f에 사용 (동그라미 색상)
-    'SUNDAY': 'rgb(255, 100, 100)', // 연한 빨간색 (일요일)
-    'SATURDAY': 'rgb(100, 100, 255)' // 연한 파란색 (토요일)
-};
+const { colorData, calendarData } = require('../../data/data.json');
 
 module.exports = {
     name: '캘린더관리',
@@ -32,7 +12,7 @@ module.exports = {
         {
             name: '행동',
             description: '행동을 선택하세요',
-            require: true,
+            required: true,
             type: ApplicationCommandOptionType.String,
             choices: [
                 { name: '추가', value: 'add', },
@@ -42,7 +22,7 @@ module.exports = {
         {
             name: '이벤트',
             description: '이벤트 명을 입력하세요',
-            require: true,
+            required: true,
             type: ApplicationCommandOptionType.String,
             choices: [
                 { name: '엘리베이터의 주', value: 'e', },
@@ -92,7 +72,7 @@ module.exports = {
                 await interaction.reply("유효하지 않은 이벤트 타입입니다.");
             }
 
-            const colorToRemove = colorMap[event];
+            const colorToRemove = colorData[event];
             let removed = removeEvents(event, colorToRemove);
 
             if (removed) {
@@ -100,14 +80,6 @@ module.exports = {
             } else {
                 await interaction.reply("해당 이벤트 타입의 이벤트가 없습니다.");
             }
-        }
-
-        // 캘린더 생성 및 이미지 파일로 저장
-        try {
-            await generateCalendarImage(interaction, today);
-        } catch (error) {
-            console.error('캘린더 이미지 생성 중 오류 발생:', error);
-            await interaction.reply('캘린더 이미지 생성 중 오류가 발생했습니다.');
         }
     },
 };
@@ -121,7 +93,7 @@ function addEventE(year, month, day) {
         if (startDate.day() !== 0 && startDate.day() !== 6) { // 주말을 제외한 날짜
             const dateStr = startDate.format('YYYY-MM-DD');
             if (!calendarData[dateStr]) calendarData[dateStr] = [];
-            calendarData[dateStr].push({ color: colorMap['e'] });
+            calendarData[dateStr].push({ color: colorData['e'] });
             daysAdded++;
         }
         startDate.add(1, 'day');
@@ -132,7 +104,7 @@ function addEventE(year, month, day) {
 function addEventF(year, month, day) {
     const date = `${year}-${String(month).padStart(2, '0')}-${String(day).padStart(2, '0')}`;
     if (!calendarData[date]) calendarData[date] = [];
-    calendarData[date].push({ color: colorMap['f'], type: 'circle' });
+    calendarData[date].push({ color: colorData['f'], type: 'circle' });
 }
 
 // 이벤트 삭제
@@ -152,150 +124,4 @@ function removeEvents(event, colorToRemove) {
     }
 
     return removed;
-}
-
-// 캘린더 이미지 생성
-async function generateCalendarImage(interaction, today) {
-    const startOfMonth = today.clone().startOf('month');
-    const endOfMonth = today.clone().endOf('month');
-    const daysInMonth = endOfMonth.date();
-    const startDay = startOfMonth.day();
-
-    const canvasWidth = 700 * scaleFactor;
-    const cellSize = 100 * scaleFactor;
-    const calendarRows = 6;
-    const canvasHeight = titleHeight + headerHeight + cellSize * calendarRows;
-
-    const canvas = createCanvas(canvasWidth, canvasHeight);
-    const ctx = canvas.getContext('2d');
-
-    // 배경색 설정
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
-
-    // 제목 설정
-    ctx.fillStyle = 'black';
-    ctx.font = `${60 * scaleFactor}px Nanum Gothic`; // 제목의 폰트 사이즈를 60으로 설정
-    ctx.textAlign = 'center';
-    ctx.textBaseline = 'middle';
-    ctx.fillText(`${today.format('MM월')}`, canvasWidth / 2, titleHeight / 2); // 제목 위치 조정
-
-    // 요일 헤더 그리기
-    drawWeekHeaders(ctx, scaleFactor, headerHeight, cellSize);
-
-    // 날짜 및 이벤트 그리기
-    drawDays(ctx, startOfMonth, daysInMonth, startDay, cellSize);
-
-    // 이미지 파일로 저장
-    const filePath = './calendar.png';
-    try {
-        const buffer = canvas.toBuffer('image/png');
-        fs.writeFileSync(filePath, buffer);
-
-        // 이미지 파일을 임베드에 추가
-        const calendarEmbed = new EmbedBuilder()
-            .setTitle('엘리베이터의 주는 초록색으로,\n자유의 날은 빨간 원으로 표시됩니다.')
-            .setColor('#FFFFFF')
-            .setImage('attachment://calendar.png');
-
-        await interaction.reply({ embeds: [calendarEmbed], files: [{ attachment: filePath, name: 'calendar.png' }] });
-    } catch (error) {
-        console.error('파일 저장 또는 메시지 전송 중 오류 발생:', error);
-        await interaction.reply('파일 저장 또는 메시지 전송 중 오류가 발생했습니다.');
-    } finally {
-        // 이미지 파일 삭제
-        try {
-            fs.unlinkSync(filePath);
-        } catch (error) {
-            console.error('파일 삭제 중 오류 발생:', error);
-        }
-    }
-}
-
-// 요일 헤더 그리기
-function drawWeekHeaders(ctx, scaleFactor, headerHeight, cellSize) {
-    const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
-    const headerBackgroundColor = 'lightgray';
-    let xOffset = 0;
-    let yOffset = titleHeight; // 제목 아래로 시작
-
-    ctx.font = `${30 * scaleFactor}px Nanum Gothic`;
-    ctx.textAlign = 'center';
-
-    daysOfWeek.forEach((day, i) => {
-        ctx.fillStyle = headerBackgroundColor;
-        ctx.fillRect(xOffset, yOffset, cellSize, cellSize);
-        ctx.strokeStyle = 'black';
-        ctx.lineWidth = 4;
-        ctx.strokeRect(xOffset, yOffset, cellSize, cellSize);
-        ctx.fillStyle = 'black';
-        ctx.fillText(day, xOffset + cellSize / 2, yOffset + cellSize / 2 + 5);
-        xOffset += cellSize;
-    });
-}
-
-// 날짜 및 이벤트 그리기
-function drawDays(ctx, startOfMonth, daysInMonth, startDay, cellSize) {
-    let xOffset = 0;
-    let yOffset = titleHeight + headerHeight; // 제목 + 헤더 아래로 시작
-    let dayCount = 1;
-    const calendarRows = 6;
-
-    for (let row = 0; row < calendarRows; row++) {
-        for (let col = 0; col < 7; col++) {
-            if (row === 0 && col < startDay) {
-                // 첫 번째 주의 빈 셀
-            } else if (dayCount <= daysInMonth) {
-                const currentDate = startOfMonth.clone().date(dayCount);
-                const currentDateStr = `${startOfMonth.year()}-${String(startOfMonth.month() + 1).padStart(2, '0')}-${String(dayCount).padStart(2, '0')}`;
-                const dayEvents = calendarData[currentDateStr] || [];
-
-                // 배경색 설정
-                let fillColor = (currentDate.day() === 0 ? colorMap['SUNDAY'] : (currentDate.day() === 6 ? colorMap['SATURDAY'] : 'white'));
-                ctx.fillStyle = fillColor;
-                ctx.fillRect(xOffset, yOffset, cellSize, cellSize);
-
-                drawEvents(ctx, dayEvents, xOffset, yOffset, cellSize);
-
-                // 날짜 텍스트
-                ctx.fillStyle = 'black';
-                ctx.font = `${24 * scaleFactor}px Nanum Gothic`;
-                ctx.textAlign = 'center';
-                ctx.fillText(dayCount, xOffset + cellSize / 2, yOffset + cellSize / 2 + 5);
-
-                dayCount++;
-            }
-
-            ctx.strokeStyle = 'black';
-            ctx.lineWidth = 4;
-            ctx.strokeRect(xOffset, yOffset, cellSize, cellSize);
-            xOffset += cellSize;
-        }
-        xOffset = 0;
-        yOffset += cellSize;
-    }
-}
-
-// 날짜 셀에 이벤트 그리기
-function drawEvents(ctx, events, xOffset, yOffset, cellSize) {
-    // 먼저 배경색 이벤트를 그립니다.
-    events.forEach(event => {
-        if (event.color && !event.type) {
-            // 배경색 이벤트
-            ctx.fillStyle = event.color;
-            ctx.fillRect(xOffset, yOffset, cellSize, cellSize);
-        }
-    });
-
-    // 이후 동그라미 이벤트를 그립니다.
-    events.forEach(event => {
-        if (event.type === 'circle') {
-            ctx.strokeStyle = colorMap['f'];
-            ctx.lineWidth = 4;
-            ctx.beginPath();
-            const circleRadius = cellSize / 3;
-            ctx.arc(xOffset + cellSize / 2, yOffset + cellSize / 2, circleRadius, 0, Math.PI * 2);
-            ctx.stroke();
-        }
-    });
 }

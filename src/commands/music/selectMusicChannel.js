7ada3a8c32c30c0af6fbabb1e8062902c6d3f02e
hanbyul0103 <all007b@naver.com>
2024-08-23 00:42:40 +0900

    feat: create new command

---
+++ src/commands/music/selectMusicChannel.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: '노래신청',
+    description: '노래 신청을 받을 채널을 선택합니다.',
+    devOnly: true,
+    testOnly: true,
+    deleted: false,
+    options: [
+        {
+            name: '채널',
+            description: '등록할 채널의 이름',
+            required: true,
+            type: ApplicationCommandOptionType.Channel,
+        },
+    ],
+
+    callback: async (client, interaction) => {
+        const channelId = interaction.options.get("채널")?.value;
+    }
+}

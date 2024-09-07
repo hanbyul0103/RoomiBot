7ab776b0fd302f580e2fa6fc09f16ef0cdf5bd14
hanbyul0103 <all007b@naver.com>
2024-09-07 10:18:29 +0900

    fix: remove //

--- src/index.js
+++ src/index.js
@@ -14,43 +14,41 @@ const client = new Client({
 
 eventHandler(client);
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'your_username',
-//     password: 'your_password',
-//     database: 'your_database'
-// });
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'your_username',
+    password: 'your_password',
+    database: 'your_database'
+})
+const spotifyApi = new SpotifyWebApi({
+    clientId: 'your_spotify_client_id',
+    clientSecret: 'your_spotify_client_secret'
+})
+async function authenticateSpotify() {
+    try {
+        const data = await spotifyApi.clientCredentialsGrant();
+        spotifyApi.setAccessToken(data.body['access_token']);
+        console.log('스포티파이 인증 성공');
+    } catch (error) {
+        console.error('스포티파이 인증 실패:', error);
+    }
+}
 
-// const spotifyApi = new SpotifyWebApi({
-//     clientId: 'your_spotify_client_id',
-//     clientSecret: 'your_spotify_client_secret'
-// });
-
-// async function authenticateSpotify() {
-//     try {
-//         const data = await spotifyApi.clientCredentialsGrant();
-//         spotifyApi.setAccessToken(data.body['access_token']);
-//         console.log('스포티파이 인증 성공');
-//     } catch (error) {
-//         console.error('스포티파이 인증 실패:', error);
-//     }
-// }
-
-// async function isSongExplicit(title, artist) {
-//     try {
-//         const result = await spotifyApi.searchTracks(`track:${title} artist:${artist}`);
-//         if (result.body.tracks.items.length > 0) {
-//             const track = result.body.tracks.items[0];
-//             return track.explicit;
-//         } else {
-//             console.log('노래를 찾을 수 없습니다.');
-//             return null;
-//         }
-//     } catch (error) {
-//         console.error('스포티파이 검색 중 오류 발생:', error);
-//         return null;
-//     }
-// }
+async function isSongExplicit(title, artist) {
+    try {
+        const result = await spotifyApi.searchTracks(`track:${title} artist:${artist}`);
+        if (result.body.tracks.items.length > 0) {
+            const track = result.body.tracks.items[0];
+            return track.explicit;
+        } else {
+            console.log('노래를 찾을 수 없습니다.');
+            return null;
+        }
+    } catch (error) {
+        console.error('스포티파이 검색 중 오류 발생:', error);
+        return null;
+    }
+}
 
 client.on('guildMemberAdd', async member => {
     const channelID = '1265224199509901374';
@@ -67,33 +65,31 @@ client.on('guildMemberAdd', async member => {
     }
 });
 
-// client.on('messageCreate', async (message) => {
-//     if (message.channel.id === "채널_ID") {
-//         if (!message.author.bot) {
-//             const [title, artist] = message.content.split(' ');
+client.on('messageCreate', async (message) => {
+    if (message.channel.id === "채널_ID") {
+        if (!message.author.bot) {
+            const [title, artist] = message.content.split(' ');
+            const isExplicit = await isSongExplicit(title, artist);
+            if (isExplicit === false) {
+                const query = `INSERT INTO songs (user_id, username, song_title, artist) VALUES (?, ?, ?, ?)`;
+                const values = [message.author.id, message.author.username, title, artist];
+                connection.query(query, values, (err) => {
+                    if (err) {
+                        console.error('데이터 삽입 중 오류 발생:', err);
+                    } else {
+                        console.log('노래 제목과 가수가 성공적으로 삽입되었습니다.');
+                    }
+                });
 
-//             const isExplicit = await isSongExplicit(title, artist);
-
-//             if (isExplicit === false) {
-//                 const query = `INSERT INTO songs (user_id, username, song_title, artist) VALUES (?, ?, ?, ?)`;
-//                 const values = [message.author.id, message.author.username, title, artist];
-
-//                 connection.query(query, values, (err) => {
-//                     if (err) {
-//                         console.error('데이터 삽입 중 오류 발생:', err);
-//                     } else {
-//                         console.log('노래 제목과 가수가 성공적으로 삽입되었습니다.');
-//                     }
-//                 });
-//             } else if (isExplicit === true) {
-//                 message.reply('이 곡은 연령 제한이 있어 신청이 불가능합니다.');
-//             } else {
-//                 message.reply('노래를 찾을 수 없거나 검색 중 오류가 발생했습니다.');
-//             }
-//         }
-//     }
-// });
+            } else if (isExplicit === true) {
+                message.reply('이 곡은 연령 제한이 있어 신청이 불가능합니다.');
+            } else {
+                message.reply('노래를 찾을 수 없거나 검색 중 오류가 발생했습니다.');
+            }
+        }
+    }
+});
 
 client.login(config.token);
 
-// authenticateSpotify();
+authenticateSpotify();

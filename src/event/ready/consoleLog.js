a74a562a35547ce4ac0769c004fa133f24b1555b
hanbyul0103 <all007b@naver.com>
2025-03-05 10:10:21 +0900

    fix: bot status

--- src/event/ready/consoleLog.js
+++ src/event/ready/consoleLog.js
@@ -1,10 +1,36 @@
 const { ActivityType } = require("discord.js");
 
+let status = [
+    {
+        name: "요청",
+        type: ActivityType.Listening
+    },
+    {
+        name: "명령어",
+        type: ActivityType.Watching
+    },
+    {
+        name: "안녕하세요, 루미입니다."
+    },
+    {
+        name: "도와드릴까요?"
+    },
+    {
+        name: "루미봇"
+    },
+    {
+        name: "기타 문의는 DM으로 부탁드려요."
+    }
+]
+
 module.exports = (client) => {
     console.log(`[!] ${client.user.tag} is ready`);
 
     client.user.setPresence({
-        activities: [{ name: "요청", type: ActivityType.Listening }],
+        activities: [{
+            name: "요청",
+            type: ActivityType.Listening
+        }],
         status: 'online'
     });
 };

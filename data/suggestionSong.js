36d373c907c59cd69b43b609d89e560bbfb38429
hanbyul0103 <all007b@naver.com>
2025-04-10 00:54:26 +0900

    fix: debug

--- data/suggestionSong.js
+++ data/suggestionSong.js
@@ -6,7 +6,7 @@ async function closeChannel(client) {
     const guild = client.guilds.fetch("795318898656018444");
     const channel = await guild.channels.fetch("951706729480081458");
     const role = await guild.roles.fetch("827187638926704641");
-
+    console.log(role);
     await channel.permissionOverwrites.edit(role, {
         SendMessages: false,
     });
@@ -16,7 +16,7 @@ async function openChannel(client) {
     const guild = client.guilds.fetch("795318898656018444");
     const channel = await guild.channels.fetch("951706729480081458");
     const role = await guild.roles.fetch("827187638926704641");
-
+    console.log(role);
     await channel.permissionOverwrites.edit(role, {
         SendMessages: true,
     });

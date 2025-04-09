369e7e357ffd8c14447c8ac6a5d72d83c23610e1
hanbyul0103 <all007b@naver.com>
2025-04-09 16:07:30 +0900

    feat: song channel permission

--- data/suggestionSong.js
+++ data/suggestionSong.js
@@ -2,4 +2,24 @@ function getSuggestionSongChannel() {
     return "1157652205327888424";
 }
 
-module.exports = { getSuggestionSongChannel };
+async function closeChannel(client) {
+    const guild = client.guilds.fetch(795318898656018444);
+    const channel = await guild.channels.fetch("951706729480081458");
+    const role = await guild.roles.fetch("827187638926704641");
+
+    await channel.permissionOverwrites.edit(role, {
+        SendMessages: false,
+    });
+}
+
+async function openChannel(client) {
+    const guild = client.guilds.fetch(795318898656018444);
+    const channel = await guild.channels.fetch("951706729480081458");
+    const role = await guild.roles.fetch("827187638926704641");
+
+    await channel.permissionOverwrites.edit(role, {
+        SendMessages: true,
+    });
+}
+
+module.exports = { getSuggestionSongChannel, closeChannel, openChannel };
--- src/event/ready/permissionReset.js
+++ src/event/ready/permissionReset.js
@@ -16,4 +16,17 @@ module.exports = async (client) => {
             }
         }
     });
+
+    cron.schedule("0 0 * * *", async () => {
+        const day = new Date().getDay();
+
+        if (day === 1 || day === 3 || day === 5) {
+            console.log("재학생 채팅 입력 권한 켬");
+            songChannel.openChannel(client);
+        }
+        else if (day === 0 || day === 2 || day === 4 || day === 6) {
+            console.log("재학생 채팅 입력 권한 끔");
+            songChannel.closeChannel(client);
+        }
+    })
 };
